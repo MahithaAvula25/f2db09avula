91d5e8b052b05713ec45b91af01b234fae93dda0
MahithaAvula25 <avula.usedu2@gmail.com>
2022-11-08 19:58:48 -0600

    Added some changes in controller file

--- controllers/icecream.js
+++ controllers/icecream.js
@@ -1,26 +1,61 @@
 var icecream = require('../models/icecream'); 
  
 // List of all Costumes 
-exports.icecream_list = function(req, res) { 
-    res.send('NOT IMPLEMENTED: icecream list'); 
-}; 
+exports.icecream_list = async function(req, res) { 
+        try{ 
+            theicecreams = await icecream.find(); 
+            res.send(theicecreams); 
+        } 
+        catch(err){ 
+            res.status(500); 
+            res.send(`{"error": ${err}}`); 
+        }   
+    }; 
  
 // for a specific Costume. 
-exports.icecream_detail = function(req, res) { 
-    res.send('NOT IMPLEMENTED: icecream detail: ' + req.params.id); 
+exports.icecream_detail = async function(req, res) { 
+    try{ 
+        theicecreams = await icecream.find(); 
+        res.send(theicecreams); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
 }; 
  
 // Handle Costume create on POST. 
-exports.icecream_create_post = function(req, res) { 
-    res.send('NOT IMPLEMENTED: icecream create POST'); 
+exports.icecream_create_post = async function(req, res) { 
+    try{ 
+        theicecreams = await icecream.find(); 
+        res.send(theicecreams); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
 }; 
  
 // Handle Costume delete form on DELETE. 
-exports.icecream_delete = function(req, res) { 
-    res.send('NOT IMPLEMENTED: icecream delete DELETE ' + req.params.id); 
+exports.icecream_delete = async function(req, res) { 
+    try{ 
+        theicecreams = await icecream.find(); 
+        res.send(theicecreams); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
 }; 
  
 // Handle Costume update form on PUT. 
-exports.icecream_update_put = function(req, res) { 
-    res.send('NOT IMPLEMENTED: icecream update PUT' + req.params.id); 
+exports.icecream_update_put = async function(req, res) { 
+    try{ 
+        theicecreams = await icecream.find(); 
+        res.send(theicecreams); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
 }; 
