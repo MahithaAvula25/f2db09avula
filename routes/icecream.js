b01c71ca63ce2ca6446eecb45088323bb9f1659b
MahithaAvula25 <avula.usedu2@gmail.com>
2022-11-14 17:09:21 -0600

    Added delete file

--- routes/icecream.js
+++ routes/icecream.js
@@ -12,5 +12,8 @@ router.get('/detail', icecream_controlers.icecream_view_one_Page);
 router.get('/create', icecream_controlers.icecream_create_Page); 
 /* GET create update page */ 
 router.get('/update', icecream_controlers.icecream_update_Page); 
+/* GET delete icecream page */ 
+router.get('/delete', icecream_controlers.icecream_delete_Page); 
+ 
  
 module.exports = router; 
