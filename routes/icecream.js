9a2b5085aa029bc8b812dd41ff34895e004ef5d1
MahithaAvula25 <avula.usedu2@gmail.com>
2022-11-30 09:05:27 -0600

    Added bonus file

--- routes/icecream.js
+++ routes/icecream.js
@@ -17,7 +17,7 @@ router.get('/icecreams/:id', icecream_controlers.icecream_detail);
 /* GET detail icecream page */ 
 router.get('/detail',secured, icecream_controlers.icecream_view_one_Page); 
 /* GET create icecream page */ 
-router.get('/create', icecream_controlers.icecream_create_Page); 
+router.get('/create',secured, icecream_controlers.icecream_create_Page); 
 /* GET create update page */ 
 router.get('/update',secured, icecream_controlers.icecream_update_Page); 
 /* GET delete icecream page */ 
